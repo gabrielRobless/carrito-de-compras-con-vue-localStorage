e0fa556d75fb80f81085ff89bcef7a8dcb8ef0e5
gabrielRobless <rgabrielsalvador1@gmail.com>
2024-02-22 17:58:02 -0600

    carrito-de-compras-con-vue-inicio

---
+++ src/components/Guitarra.vue
@@ -0,0 +1,32 @@
+<script setup>
+import {ref} from 'vue'
+
+const props = defineProps({
+    guitarra: {
+        type: Object,
+        required: true
+    }
+
+})
+
+
+</script>
+
+
+<template>
+    <div class="col-md-6 col-lg-4 my-4 row align-items-center">
+        <div class="col-4">
+            <img class="img-fluid" v-bind:src="'/img/' + guitarra.imagen + '.jpg'"
+                :alt="'imagen guitarra ' + guitarra.nombre">
+        </div>
+        <div class="col-8">
+            <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+            <p>{{ guitarra.descripcion }}</p>
+            <p class="fw-black text-primary fs-3">${{ guitarra.precio }}</p>
+            <button type="button" class="btn btn-dark w-100 " v-on:click="">Agregar al Carrito</button>
+        </div>
+    </div><!-- FIN GUITARRA -->
+</template>
+
+
+<style lang="scss" scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import {ref, reactive} from 'vue'
+
+    
+
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped>
+
+</style>
