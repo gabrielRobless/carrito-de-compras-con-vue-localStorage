31b788734e186ca82d9dce07a57a01a7de35083d
gabrielRobless <rgabrielsalvador1@gmail.com>
2024-02-23 14:02:59 -0600

    organicacion de header y footer

--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -1,5 +1,7 @@
 <script setup>
-import {ref} from 'vue'
+
+
+const numero = ref(0)
 
 const props = defineProps({
     guitarra: {
@@ -8,6 +10,9 @@ const props = defineProps({
     }
 
 })
+
+defineEmits(['agregar-carrito'])
+
 
 
 </script>
@@ -21,9 +26,10 @@ const props = defineProps({
         </div>
         <div class="col-8">
             <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
+        
             <p>{{ guitarra.descripcion }}</p>
             <p class="fw-black text-primary fs-3">${{ guitarra.precio }}</p>
-            <button type="button" class="btn btn-dark w-100 " v-on:click="">Agregar al Carrito</button>
+            <button type="button" class="btn btn-dark w-100 " @click="$emit('agregar-carrito', guitarra) ">Agregar al Carrito</button>
         </div>
     </div><!-- FIN GUITARRA -->
 </template>
