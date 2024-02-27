9856a1500f3c1d2b54dba17fcd4c1e1d07da5973
gabrielRobless <rgabrielsalvador1@gmail.com>
2024-02-26 18:31:15 -0600

    carrito terminado

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,85 @@
 <script setup>
-import { ref, reactive, onMounted } from "vue";
+import { ref, reactive, onMounted, watch } from "vue";
 import { db } from "./data/guitarras";
 import Guitarra from "./components/Guitarra.vue";
-import Header from "./components/Header.vue";
+import Header from "./components/Headerr.vue";
 import Footer from "./components/Footer.vue";
 
 const guitarras = ref([]);
 const carrito = ref([]);
+const guitarra = ref({});
+
+watch(carrito, ()=>{
+  guardarLocalStorage();
+}, {deep: true})
 
 onMounted(() => {
   guitarras.value = db;
+  guitarra.value =db[3]
+  const carritoStorage = localStorage.getItem('carrito')
+  if(carritoStorage){
+    carrito.value = JSON.parse(carritoStorage)
+  }
 });
 
+const guardarLocalStorage =() => {
+  localStorage.setItem("carrito", JSON.stringify(carrito.value));
+}
+
 const agregarCarrito = (guitarra) => {
-  guitarra.cantidad = 1;
-  carrito.value.push(guitarra);
+  const existeCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
+  if(existeCarrito >= 0){
+    carrito.value[existeCarrito].cantidad++
+  }else{
+      guitarra.cantidad =1 ;
+      carrito.value.push(guitarra);
+
+  }
+
+ 
+
 };
+
+const decrementarCantidad =(id ) =>{
+  const index = carrito.value.findIndex(producto => producto.id === id)
+  if(carrito.value[index].cantidad<= 1) return
+  carrito.value[index].cantidad--
+ 
+
+}
+
+const incrementarCantidad = (id) =>{
+  const index = carrito.value.findIndex(producto => producto.id === id)
+  if(carrito.value[index].cantidad >= 5) return
+  carrito.value[index].cantidad++
+  
+
+}
+
+const eliminarProducto = (id) =>{
+  carrito.value = carrito.value.filter(producto => producto.id !== id)
+
+
+}
+
+const vaciarCarrito = ( ) => {
+  carrito.value = []
+}
+
+
+
 </script>
 
 <template>
   <Header
     :carrito="carrito"
-  />
+    :guitarra ="guitarra"
+    @decrementar-cantidad="decrementarCantidad"
+    @incrementar-cantidad="incrementarCantidad"
+    @agregar-carrito="agregarCarrito"
+    @eliminar-producto="eliminarProducto"
+    @vaciar-carrito="vaciarCarrito"
+    />
 
   <main class="container-xl mt-5">
         <h2 class="text-center">Nuestra Colección</h2>
